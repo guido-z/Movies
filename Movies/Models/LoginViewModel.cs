b95d3d6aabb64f68aff0a5aaea68e9c299de71ae
Guido <guido.zaglul@gmail.com>
2016-12-31 02:31:09 -0300

    Agregué la pantalla de login.

---
+++ Movies/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Movies.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Debe ingresar un nombre de usuario.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar una contraseña.")]
+        public string Password { get; set; }
+    }
+}
